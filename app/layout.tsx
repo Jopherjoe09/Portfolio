520a3bd47970d69a07e154c6a07d08bcb1aedce9
Jopher Joe <joejopher@gmail.com>
2025-09-01 10:28:57 +0800

    Feat(JJP-86d05c8qg): Added a Scroll Animation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { GeistSans } from "geist/font/sans"
-import { GeistMono } from "geist/font/mono"
-import { Analytics } from "@vercel/analytics/next"
-import { Suspense } from "react"
-import "./globals.css"
+import type React from "react";
+import type { Metadata } from "next";
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
+import { Analytics } from "@vercel/analytics/next";
+import { Suspense } from "react";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Jopher Joe Ribo - Web Developer Portfolio",
   description:
     "Aspiring Web Developer crafting responsive websites with heart and logic. Showcasing projects in React, JavaScript, and modern web technologies.",
   generator: "v0.app",
-}
+  icons: {
+    icon: "/logo-shesh.png",
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -25,5 +28,5 @@ export default function RootLayout({
         <Analytics />
       </body>
     </html>
-  )
+  );
 }
